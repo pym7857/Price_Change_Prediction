c8c21f74af102651e6549d5d859b33ba821b6f67
pym7857 <pym7857@naver.com>
2019-11-16 23:40:37 +0900

    first

---
+++ Flask Web Server/server.py
@@ -0,0 +1,48 @@
+from flask import Flask, render_template, request
+
+import datetime
+import tensorflow as tf
+import numpy as np
+
+app = Flask(__name__)
+
+X = tf.compat.v1.placeholder(tf.float32, shape=[None, 4])
+Y = tf.compat.v1.placeholder(tf.float32, shape=[None, 1])
+W = tf.Variable(tf.random.normal([4, 1]), name="weight")
+b = tf.Variable(tf.random.normal([1]), name="bias")
+
+hypothesis = tf.matmul(X, W) + b
+
+# 저장된 학습모델 불러오기 위한 초기화
+saver = tf.compat.v1.train.Saver()
+model = tf.compat.v1.global_variables_initializer()
+sess = tf.compat.v1.Session()
+sess.run(model)
+
+save_path = "../Python Code/saved.cpkt"
+saver.restore(sess, save_path)      # 해당 세션에 저장된 학습모델을 불러온다.
+
+@app.route("/", methods=['GET', 'POST'])
+def index():
+    if request.method == 'GET':
+      return render_template('index.html')
+    if request.method == 'POST':
+        avg_temp = float(request.form['avg_temp'])
+        min_temp = float(request.form['min_temp'])
+        max_temp = float(request.form['max_temp'])
+        rain_fall = float(request.form['rain_fall'])
+
+    price = 0   # price 변수 생성
+
+    data = ((avg_temp, min_temp, max_temp, rain_fall),)
+    arr = np.array(data, dtype=np.float32)
+
+    x_data = arr[0: 4]
+    dict = sess.run(hypothesis, feed_dict={X: x_data})
+
+    price = dict[0]
+    return render_template('index.html', price=price)   # 서버로 price 변수값 보냄
+
+if __name__ == '__main__':
+    app.run(debug=True)
+    #app.run(host = '0.0.0.0')
--- README.md
+++ README.md
@@ -1,2 +1,86 @@
-# Price_Change_Prediction
-use Tensorflow and Keras
+# 주제
+텐서플로우를 이용한 다변인 선형회귀를 이용한 가격예측
+<br>
+LSTM 딥러닝을 이용한 미래 가격예측
+
+## 설계도
+![4](./SampleImage/4.PNG) 
+
+## Web Site
+http://18.221.85.158:5000/
+
+## 텐서플로우 소개 
+```
+설명 
+```
+
+## LSTM 소개
+```
+설명
+
++ 총 7486 개의 엑셀 데이터
++ 그래프 추가 
+```
+
+## 피어슨 상관계수
+```
+피어슨 상관계수를 이용하여 연관이 높은 변인만을 추출하여 사용합니다.
+```
+
+## 서버 실행
+```
+# 깃 허브에서 소스코드를 다운로드 받습니다.
+https://github.com/pym7857/pricePrediction.git
+
+# 받은 프로젝트 폴더로 이동합니다.
+cd pricePrediction
+
+# 플라스크 웹 서버 폴더로 이동합니다.
+cd "Flask Web Server"
+
+# 웹 서버를 실행합니다.
+python server.py
+```
+
+## 텐서플로우 데이터 학습 모델 생성
+```
+# 프로젝트 폴더에서 파이썬 폴더로 이동합니다.
+cd "Python Code"
+
+# 엑셀(Excel) 파일로 학습을 수행합니다.
+python offline_save.py
+
+# (옵션) 학습된 데이터를 확인합니다.
+python "user_input.py"
+>> 평균 온도: -2.7
+>> 최저 온도: -6.6
+>> 최고 온도: 2.0
+>> 강수량:  0.1
+[2000.2086]
+
+# 이후에 생성된 모델 파일을 웹 서버의 model 폴더에 붙여넣기 하면 적용됩니다.
+```
+
+## 핵심사항
+```
+* 텐서플로우
+# offline_save.py: 다변인 선형회귀 모델 개발 & 데이터 저장 (model 폴더에 저장)
+# user_input.py: 데이터 불러오기 & 사용자 입력
+# Flask의 폴더는 model / static / templates로 나누어 구성 
+# index.html에서 데이터 입력부분은 form 태그로 POST 방식으로 데이터 전송
+# server.py: 플라스크(Flask)를 이용해 웹서버 연동 (port:5000)
+
+* LSTM
+설명
+```
+
+## AWS EC2를 이용해 hosting
+```
+설명(과정)
+# 1.
+# 2.
+# 3.
+```
+
+<br>
+<br>
